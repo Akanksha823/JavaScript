87b7df99a7417179bfde61a7f56f6641429baea1
Akanksha823 <vermaakanksha823@gmail.com>
2022-05-08 16:47:13 +0530

    first commit

---
+++ First.js
@@ -0,0 +1,41 @@
+//string
+let Name = "Akanksha";
+console.log("My String is " + Name);
+console.log("Data type is " + (typeof Name));
+
+//Numbers
+// let marks=76;
+// Console.log("Data type is" + (typeof marks));
+
+//Boolean
+let isDriver=true;
+console.log("Data type is " + (typeof isDriver));
+
+//Null
+let nullvar = null;
+console.log("Data type is " + (typeof nullvar));
+
+//undefined
+let undef= undefined;
+console.log("Data type is " + (typeof undef))
+
+//Reference data type
+myarr = [1,2,3,4,false,"string"];
+console.log("Data type is " + (typeof myarr));
+
+//object literals
+let stMarks = {
+shubham: 45,
+Rohit:76,
+Gungun: 97
+};
+console.log(typeof stMarks);
+
+function findname() {
+    
+}
+console.log(typeof findname);
+
+let date= new Date();
+console.log(typeof date);
+
