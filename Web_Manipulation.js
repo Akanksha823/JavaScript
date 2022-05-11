84b631e22cb710da4b992592d96b10858f779cd7
Akanksha823 <vermaakanksha823@gmail.com>
2022-05-11 19:56:32 +0530

    Web_Manipulation commit

---
+++ Web_Manipulation.js
@@ -0,0 +1,18 @@
+console.log("DOM");
+
+let a = window;
+console.log(a);
+
+let b = document;
+console.log(b);
+alert('stay update')
+
+// b= prompt('This will distroy your computer');
+// console.log(b);
+
+// b=confirm('Are you sure you want to delete this page?')
+
+console.log(b)
+b=location.toString();
+b=window.history;
+console.log(b);
