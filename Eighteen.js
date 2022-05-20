5615c8dd4ca3235616da55360693f42cb6248c9a
Akanksha Verma <67753600+Akanksha823@users.noreply.github.com>
2022-05-20 22:54:49 +0530

    Add files via upload

---
+++ Eighteen.js
@@ -0,0 +1,32 @@
+console.log("trial!");
+
+let today  = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+let otherDate = new Date();
+otherDate = new Date ('8-3-1967 07:45:06');
+// otherDate = new Date ('09/16/1976');
+console.log(otherDate);
+
+let a;
+a = otherDate.getDay();
+a = otherDate.getDate();
+a = otherDate.getTime();
+a = otherDate.getHours();
+a = otherDate.getMinutes();
+// a = otherDate.getSecond();
+a = otherDate.getMonth();
+console.log(a);
+
+
+// otherDate = setDate(23);
+// otherDate = setMonth(4);
+// otherDate = setFullYear(2010);
+// otherDate = setMiniutes(2);
+// otherDate = setHours(1);
+// otherDate = setSeconds(3);
+console.log(otherDate);
+
+
+
